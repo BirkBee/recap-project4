5fb66f1905cccee49d4df40fb95079e37d42cce3
Birk Beese <birk.beese@gmail.com>
2023-10-23 16:27:40 +0200

    solved task1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { uid } from "uid";
+import { useState } from "react";
 import Form from "./components/Form";
 import React from "react";
+
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  function handleAddActivity(data) {
+    // console.log(data);
+    setActivities([{ ...activities, data, id: uid() }]);
+  }
+  console.log(activities);
   return (
     <div>
-      <Form />
+      <Form onAddActivity={handleAddActivity} />
     </div>
   );
 }
